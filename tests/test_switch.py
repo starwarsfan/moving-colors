96f831bfb792f4bc3ce269f5ecc315ded3305d49
Yves Schumann <y.schumann@proton.me>
2025-12-29 17:41:50 +0100

    Update tests

--- tests/test_switch.py
+++ tests/test_switch.py
@@ -1,14 +1,6 @@
 """Test moving_colors switch."""
 
-from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
-from homeassistant.const import (
-    ATTR_ENTITY_ID,
-    EVENT_HOMEASSISTANT_STARTED,
-    SERVICE_TURN_OFF,
-    SERVICE_TURN_ON,
-    STATE_OFF,
-    STATE_ON,
-)
+from homeassistant.const import EVENT_HOMEASSISTANT_STARTED, STATE_OFF, STATE_ON
 from homeassistant.core import HomeAssistant
 
 
@@ -27,10 +19,11 @@ async def test_switch_setup(hass: HomeAssistant, mock_config_entry, mock_light) 
     state = hass.states.get(entity_id)
 
     assert state is not None, f"Switch {entity_id} was not created"
+    assert state.state in (STATE_ON, STATE_OFF), f"Switch has invalid state: {state.state}"
 
 
-async def test_switch_turn_on_off(hass: HomeAssistant, mock_config_entry, mock_light) -> None:
-    """Test turning switch on and off."""
+async def test_switch_states(hass: HomeAssistant, mock_config_entry, mock_light) -> None:
+    """Test that all switches are created with valid states."""
     mock_config_entry.add_to_hass(hass)
 
     hass.bus.async_fire(EVENT_HOMEASSISTANT_STARTED)
@@ -39,39 +32,16 @@ async def test_switch_turn_on_off(hass: HomeAssistant, mock_config_entry, mock_l
     assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
-    entity_id = "switch.test_moving_colors_enable_moving_colors"
+    # Check all expected switches exist
+    expected_switches = [
+        "switch.test_moving_colors_debug_mode",
+        "switch.test_moving_colors_enable_moving_colors",
+        "switch.test_moving_colors_random_limits",
+        "switch.test_moving_colors_activate_default_mode",
+        "switch.test_moving_colors_star_from_current_color",
+    ]
 
-    # Just verify the switch can be controlled, regardless of initial state
-    initial_state = hass.states.get(entity_id)
-    assert initial_state is not None
-    initial_value = initial_state.state
-
-    # Try toggling
-    target_service = SERVICE_TURN_OFF if initial_value == STATE_ON else SERVICE_TURN_ON
-    target_state = STATE_OFF if initial_value == STATE_ON else STATE_ON
-
-    await hass.services.async_call(
-        SWITCH_DOMAIN,
-        target_service,
-        {ATTR_ENTITY_ID: entity_id},
-        blocking=True,
-    )
-    await hass.async_block_till_done()
-
-    new_state = hass.states.get(entity_id)
-    assert new_state is not None
-    assert new_state.state == target_state
-
-    # Toggle back
-    target_service = SERVICE_TURN_ON if target_state == STATE_OFF else SERVICE_TURN_OFF
-    await hass.services.async_call(
-        SWITCH_DOMAIN,
-        target_service,
-        {ATTR_ENTITY_ID: entity_id},
-        blocking=True,
-    )
-    await hass.async_block_till_done()
-
-    final_state = hass.states.get(entity_id)
-    assert final_state is not None
-    assert final_state.state == initial_value
+    for entity_id in expected_switches:
+        state = hass.states.get(entity_id)
+        assert state is not None, f"Switch {entity_id} was not created"
+        assert state.state in (STATE_ON, STATE_OFF), f"Switch {entity_id} has invalid state"
